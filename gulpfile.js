82ad6af2b66089a46564affc1c33a9b25559b2e9
Evan Goode <mail@evangoo.de>
2016-01-21 22:12:20 -0500

    stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var jade = require('gulp-jade');
+var plumber = require('gulp-plumber');
 
 var paths = {
   sass: ['**/*.sass', '**/*.scss', '!node_modules/**'],
@@ -13,15 +14,17 @@ gulp.task('default', ['sass', 'jade']);
 
 gulp.task('sass', function() {
   return gulp.src(paths.sass)
-    .pipe(sass().on('error', sass.logError))
+    .pipe(plumber())
+    .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(gulp.dest('.'))
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('jade', function() {
   return gulp.src(paths.jade)
+    .pipe(plumber())
     .pipe(jade())
-    .pipe(gulp.dest('.'))
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function() {
