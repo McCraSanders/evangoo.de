3bcc3a968b958a671dbd35ccfdb3b9a8e8147a4f
Evan Goode <mail@evangoo.de>
2016-11-06 15:59:46 -0500

    rm pebble and pinkberry

--- pinkberry/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$address = $_SERVER['REMOTE_ADDR'];
-	exec("bash pushbullet.sh $address");
-	header('Location: http://evangoo.de/pinkberry/data.csv');
-	die();
-?>
--- pinkberry/pushbullet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-key="o.Q06F9uAxsfG4eGQ2QFO1Ke0u3O8ebtcN"
-title="pinkberry"
-message="$1"
-
-curl -u $key: https://api.pushbullet.com/v2/pushes -d type=note -d title="$title" -d body="$message"
